4c03fcc7a000c6489f3324d358c6ff8d52e3cbb9
WellingtonSilv <48716017+WellingtonSilv@users.noreply.github.com>
2019-09-24 18:07:00 -0300

    New argument and comments added
    I'm update travel() function with the new argument: fisrt_row.na
    
    In the last version of this function we recieve a ERRO message when we try to run in a dataframe or matriz that have missing values on
    the first row, to solve that, i insert a new argument: first_row.na that recieve TRUE or FALSE.
    
    If the first_row.na argument if recieves TRUE the function replace first row missing values by element selected in "element argument",
    if recieve FALSE the function ignore first_row.na argument and run normally.
    
    the "element argument" can be used to replace missing values also using last.na=FALSE and first_row.na=TRUE with no problem,
    the function will run and replace all missing values by selected element.
    
    to easily understening i added some comments to the code.

--- travel.R
+++ travel.R
@@ -1,8 +1,28 @@
-travel <-  function(data,element = 0,last.na = FALSE){
+travel <-  function(data,element = 0,last.na = FALSE,first_row.na=F){
+  
+  #verifying first_row.na
+  if(first_row.na==T){
+    
+    #loop to replace first row NA's by selected element
+    for (i in 1:length(colnames(data))) {
+      if(is.na(data[1,i])==T){
+        data[1,i] <- element
+      }
+      i=+1
+    }
+  }
+  
+  #difyning variables to verify and print final messages
   x = 0
   y = 0
+  
+  #loop to go through rows
   for (i in 1:dim(data)[1]) {
+    
+    #lopp to go through columns
     for (j in 1:dim(data)[2]) {
+      
+      #searching for missing values to replace
       if(is.na(data[i,j])){
         if(last.na==TRUE){
           data[i,j] <-  data[i-1,j]
@@ -44,6 +64,8 @@ travel <-  function(data,element = 0,last.na = FALSE){
     }
     i = i+1
   }
+  
+  #print final messages
   if(x!=0 && y==0){
     cat("All missing values(NA, NaN, inf and NULL) have been replaced by:",element,"\n")
   }else if(x!=0 && y!=0){
